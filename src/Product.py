58d08f36296f8d54d5fa938b5a1e8cdf12863f5e
Rahul Sharma <rahulsharma1092@gmail.com>
2019-12-12 18:46:46 +0530

    Added Product and VendingMachine classes

---
+++ src/Product.py
@@ -0,0 +1,8 @@
+class Product():
+    """
+    Represents a Product
+    """
+    def __init__(self, product_id, product_name, price):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.price = price
---
+++ src/VendingMachine.py
@@ -0,0 +1,35 @@
+class VendingMachine():
+    """
+    Simulates Vending Machine Operations
+    """
+    def __init__(self):
+        """
+        Initializes the Vending Machine
+        """
+        pass
+
+    def initialization_data(self):
+        """
+        :return: Data used for initializing Vending Machine 
+        """
+        pass
+    
+    def is_sale_possible(self):
+        """
+        :return: True if possible, Else False
+        """
+
+    def make_sale(self):
+        """
+        :return: Updates the vending machine's state
+        """
+
+    def handle_input(self):
+        """
+        Handles user input and then takes relevant actions
+        """
+
+    def produce_display(self):
+        """
+        Creates a user interface for the vending machine
+        """
